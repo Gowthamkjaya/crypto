5762bc72334ea82f1a762c567bd6c281762b59be
Gowtham K Jayachandiran <63406677+Gowthamkjaya@users.noreply.github.com>
2025-12-22 20:23:52 -0800

    Update newnewnew_xrp.py

--- newnewnew_xrp.py
+++ newnewnew_xrp.py
@@ -14,7 +14,9 @@ import pandas as pd
 # 🔧 CONFIGURATION
 # ==========================================
 
-PRIVATE_KEY = "0xbbd185bb356315b5f040a2af2fa28549177f3087559bb76885033e9cf8e8bf34"
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+if not PRIVATE_KEY:
+    raise ValueError("❌ PRIVATE_KEY not found in environment variables!")
 POLYMARKET_ADDRESS = "0xC47167d407A91965fAdc7aDAb96F0fF586566bF7"
 
 # Strategy Settings - "The Fallen Favorite"
@@ -605,4 +607,5 @@ class FallenFavoriteBot:
 
 if __name__ == "__main__":
     bot = FallenFavoriteBot()
+
     bot.run()
